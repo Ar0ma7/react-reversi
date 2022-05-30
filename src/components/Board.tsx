80995a76b6651df0133d51f444f1501e28bbf8ac
Aroma <aroma.xxx.777@gmail.com>
2022-05-31 06:54:04 +0900

    feat: 置けるマスチェック

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,10 +4,10 @@ import styled from '@emotion/styled'
 import SquareState from '@/types/SquareState'
 
 type BoardProps = {
-  /* 偶数 */
-  size: number
   /* 最初のターン - 0: 自分, 1: 相手 */
   firstTurn: number
+  /* 盤面サイズ（偶数） */
+  size: number
 }
 
 type BoardType = SquareState[][]
@@ -30,9 +30,108 @@ const getInitializedBoard = (size: number): BoardType => {
   return board
 }
 
-export const Board: React.FC<BoardProps> = React.memo(({ size, firstTurn }) => {
-  const [board, setBoard] = useState<BoardType>(getInitializedBoard(size))
+const getMovableDir = (board: BoardType, currentValue: number): number[][] => {
+  return board.map((row, rowIdx) => row.map((value, colIdx) => checkMobility(board, rowIdx, colIdx, currentValue)))
+}
+const getMovablePos = (board: BoardType, currentValue: number): boolean[][] => {
+  return board.map((row, rowIdx) =>
+    row.map((value, colIdx) => {
+      const dir = checkMobility(board, rowIdx, colIdx, currentValue)
+      return dir !== 0 ? true : false
+    }),
+  )
+}
+
+const checkMobility = (board: BoardType, row: number, col: number, value: number): number => {
+  let direction = 0
+  // 既に石があるか
+  if (board[row][col] !== 0) return direction
+  // 左
+  if (board[row - 1][col] === -value) {
+    let rowTemp = row - 2
+    const colTemp = col
+    while (board[rowTemp][colTemp] === -value) {
+      rowTemp -= 1
+    }
+    if (board[rowTemp][colTemp] === value) direction = direction | 1
+  }
+  // 左上
+  if (board[row - 1][col - 1] === -value) {
+    let rowTmp = row - 2
+    let colTmp = col - 2
+    while (board[rowTmp][colTmp] === -value) {
+      rowTmp -= 1
+      colTmp -= 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 2
+  }
+  // 上
+  if (board[row][col - 1] === -value) {
+    const rowTmp = row
+    let colTmp = col - 2
+    while (board[rowTmp][colTmp] === -value) {
+      colTmp -= 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 4
+  }
+  // 右上
+  if (board[row + 1][col - 1] === -value) {
+    let rowTmp = row + 2
+    let colTmp = col - 2
+    while (board[rowTmp][colTmp] === -value) {
+      rowTmp += 1
+      colTmp -= 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 8
+  }
+  // 右
+  if (board[row + 1][col] === -value) {
+    let rowTmp = row + 2
+    const colTmp = col
+    while (board[rowTmp][colTmp] === -value) {
+      rowTmp += 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 16
+  }
+  // 右下
+  if (board[row + 1][col + 1] === -value) {
+    let rowTmp = row + 2
+    let colTmp = col + 2
+    while (board[rowTmp][colTmp] === -value) {
+      rowTmp += 1
+      colTmp += 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 32
+  }
+  // 下
+  if (board[row][col + 1] === -value) {
+    const rowTmp = row
+    let colTmp = col + 2
+    while (board[rowTmp][colTmp] === -value) {
+      colTmp += 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 64
+  }
+  // 左下
+  if (board[row - 1][col + 1] === -value) {
+    let rowTmp = row - 2
+    let colTmp = col + 2
+    while (board[rowTmp][colTmp] === -value) {
+      rowTmp -= 1
+      colTmp += 1
+    }
+    if (board[rowTmp][colTmp] === value) direction = direction | 128
+  }
+  return direction
+}
+
+export const Board: React.FC<BoardProps> = React.memo(({ firstTurn, size }) => {
   const [turn, setTurn] = useState<number>(firstTurn)
+  const [board, setBoard] = useState<BoardType>(getInitializedBoard(size))
+  const [movableDir, setMovableDir] = useState<number[][]>(getMovableDir(board, turn % 2 === 0 ? 1 : -1))
+  const [movablePos, setMovablePos] = useState<boolean[][]>(getMovablePos(board, turn % 2 === 0 ? 1 : -1))
+
+  const sizes = [...Array(size + 2)].fill('50px')
 
   const items = board.map((row, rowIdx) =>
     row.map((col, colIdx) => {
@@ -45,10 +144,8 @@ export const Board: React.FC<BoardProps> = React.memo(({ size, firstTurn }) => {
     }),
   )
 
-  const sizes = [...Array(size + 2)].fill('50px')
-
   const useForceUpdate = (): (() => void) => {
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState(0) // eslint-disable-line no-unused-vars
     return () => setValue((value) => value + 1)
   }
 
@@ -61,19 +158,31 @@ export const Board: React.FC<BoardProps> = React.memo(({ size, firstTurn }) => {
    * @param {number} col
    */
   const handleClick = (row: number, col: number) => {
-    if (board[row][col] === 0) {
-      const newBoard = board
-      newBoard[row][col] = turn % 2 === 0 ? 1 : -1
-      setBoard(newBoard)
+    const newBoard = board
+    const nextStone = turn % 2 === 0 ? 1 : -1
+    if (getMovablePos(board, nextStone)[row][col]) {
+      newBoard[row][col] = nextStone
       setTurn(turn + 1)
+      setBoard(newBoard)
+      setMovableDir(getMovableDir(newBoard, -nextStone))
+      setMovablePos(getMovablePos(newBoard, -nextStone))
       forceUpdate()
     }
   }
 
   return (
-    <StyledGridContainer columns={sizes} rows={sizes} areas={board.map((r, i) => r.map((c, j) => `area${i}${j}`))}>
-      {items}
-    </StyledGridContainer>
+    <>
+      next: {turn % 2 === 0 ? 1 : -1}
+      <StyledGridContainer columns={sizes} rows={sizes} areas={board.map((r, i) => r.map((c, j) => `area${i}${j}`))}>
+        {items}
+      </StyledGridContainer>
+      {movableDir.map((r, i) => (
+        <p key={i}>{r.map((c) => `　 ${c} 　`)}</p>
+      ))}
+      {movablePos.map((r, i) => (
+        <p key={i}>{r.map((c) => `${c} 　`)}</p>
+      ))}
+    </>
   )
 })
 Board.displayName = 'Board'
