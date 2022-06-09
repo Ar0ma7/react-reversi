b25c02c173a3b4fa5fb0830e9b69d82fdce33aa0
Aroma <aroma.xxx.777@gmail.com>
2022-06-09 09:56:02 +0900

    feat: cpu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Board } from '@/components/Board'
 import { Setting } from '@/components/Setting'
-import { useAppSelector } from '@/modules'
+import { AppDispatch, boardSlice, playerSlice, useAppDispatch, useAppSelector } from '@/modules'
 import { BoardType, Stone } from '@/types/global'
+import { getCpuFlippedBoard } from '@/scripts/functions'
 
 export default function Home() {
+  console.log('render home')
+  const dispatch: AppDispatch = useAppDispatch()
+  const { setCurrentTurn } = playerSlice.actions
+  const { setBoard } = boardSlice.actions
   const board: BoardType = useAppSelector((state) => state.board.board)
+  const playerStone: Stone = useAppSelector((state) => state.player.playerStone)
   const currentTurn: Stone = useAppSelector((state) => state.player.currentTurn)
+
+  if (currentTurn !== playerStone) {
+    setTimeout(() => {
+      dispatch(setBoard([...getCpuFlippedBoard(board, currentTurn)]))
+      dispatch(setCurrentTurn(currentTurn === 1 ? -1 : 1))
+    }, 1000);
+  }
 
   return (
     <>
